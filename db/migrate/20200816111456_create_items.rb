3753216126e808419d93291ca9180813b2da4a1f
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-16 20:26:47 +0900

    db追加

---
+++ db/migrate/20200816111456_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :image
+      t.integer :price
+      t.string :description
+      t.boolean :is_display, default: true
+      t.integer :display_order
+
+      t.timestamps
+    end
+  end
+end
