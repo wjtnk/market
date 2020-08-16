f1486918228de37fd60780cf3197959ed2257933
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-16 20:33:52 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create!(
+    [
+        {
+            name: 'サンプルItem1',
+            image: 'sample1.png',
+            price: 1000,
+            description: 'サンプルItem1の説明文',
+            is_display: 1,
+            display_order: 1
+        },
+        {
+            name: 'サンプルItem2',
+            image: 'sample2.png',
+            price: 2000,
+            description: 'サンプルItem2の説明文',
+            is_display: 1,
+            display_order: 2
+        },
+        {
+            name: 'サンプルItem3',
+            image: 'sample1.png',
+            price: 3000,
+            description: 'サンプルItem3の説明文',
+            is_display: 1,
+            display_order: 3
+        }
+    ]
+)
