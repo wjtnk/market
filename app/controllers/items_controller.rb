b83eae5f14afc4637f697577460e06f085c9f48d
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-17 22:18:35 +0900

    buyアクション作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def buy
+    byebug
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'items#index'
+
+  get  "items/:item_id/buy" , to: "items#buy" , as: 'items_buy'
 end
