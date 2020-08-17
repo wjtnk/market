95a407dbb6a67fe0c5166287985eaca1d8765c33
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-17 21:43:05 +0900

    userのカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # deviseで追加でUserテーブルに入れたいカラム
+  def configure_permitted_parameters
+    added_attrs = [:name, :address]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
+
 end
