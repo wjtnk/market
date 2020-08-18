baaed7ea38696447339ee8f08c14bfad95aa7ee1
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-18 23:23:55 +0900

    カート一覧に個数追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,32 +1,33 @@
 class Item < ApplicationRecord
 
   # キャッシュからItemを取得
+  # user_idをkeyとして、{itemのid => itemの個数}
+  # e.g){ 1 => 500, 2 => 100, 3 =>10} の時、itemのidが1のものが500個、itemのidが2のものが100個、itemのidが3のもの10個
   def self.get_item_info(user_id)
-    cart = Rails.cache.read(user_id)
-    return cart.blank? ? [] : Item.where(id: cart[:items])
+    items = Rails.cache.read(user_id)
+    return items.blank? ? [] : items
   end
 
   # キャッシュにitemを追加
-  # user_idをkeyとして、{itemのid => itemの個数}
-  # e.g){ 1 => 500, 2 => 100, 3 =>10} の時、itemのidが1のものが500個、itemのidが2のものが100個、itemのidが3のもの10個
+  # 追加する形式はget_item_infoメソッドを参照
   def self.add_item(user_id, item_id)
-    cart = Rails.cache.read(user_id)
+    item = Rails.cache.read(user_id)
 
     # itemが何もなかったら新たに配列に書き込み
-    if cart.blank?
+    if item.blank?
       Rails.cache.write(user_id, {item_id => 1})
       return
     end
 
     # itemが新しいものだったら
-    if cart[item_id].blank?
-      cart[item_id] = 1
-      Rails.cache.write(user_id, cart)
+    if item[item_id].blank?
+      item[item_id] = 1
+      Rails.cache.write(user_id, item)
       return
     end
 
-    cart[item_id] +=  1
-    Rails.cache.write(user_id, cart)
+    item[item_id] +=  1
+    Rails.cache.write(user_id, item)
 
   end
 
