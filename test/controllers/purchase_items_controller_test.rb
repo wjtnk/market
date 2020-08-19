1c8558e96a53c8d4e6163340ba404cf605668d3c
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-19 09:23:28 +0900

    リファクタ

--- test/controllers/purchase_items_controller_test.rb
+++ test/controllers/purchase_items_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PurchaseHistoriesControllerTest < ActionDispatch::IntegrationTest
+class PurchaseItemsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
