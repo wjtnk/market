d130ebd73bcec794497e8414db0c7a62424ebcac
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-19 23:42:28 +0900

    キャッシュに保存している商品を購入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,23 @@ class OrdersController < ApplicationController
     @item_total_price, @delivery_fee, @cash_on_delivery_fee, @order_total_price = Order.get_order_each_prices(@items, @item_count)
   end
 
+  # キャッシュに保存している商品を購入
   def create
+    items = Item.get_items(current_user.id)
+    item_count = Order.get_item_count(items)
+    item_total_price, delivery_fee, cash_on_delivery_fee, order_total_price = Order.get_order_each_prices(items, item_count)
+    Order.create!(
+        delivery_fee: delivery_fee,
+        cash_on_delivery_fee: cash_on_delivery_fee,
+        total_price: order_total_price,
+        user_id: current_user.id
+    )
+
+    # TODO
+    # purchase_itemsを保存
+
+    flash[:notice] = "購入ありがとうございます"
+    redirect_to root_path
   end
 
 end
--- db/migrate/20200818145312_create_orders.rb
+++ db/migrate/20200818145312_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :cash_on_delivery_fee
       t.integer :delivery_fee
+      t.integer :cash_on_delivery_fee
       t.integer :total_price
       t.references :user, foreign_key: true
 
