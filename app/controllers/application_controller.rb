c77155427a55ea67f2f9f5a0e1fb31cadcfa38f4
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-21 18:31:54 +0900

    ユーザー情報編集修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [:name, :default_address, :default_deliver_time]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 end
