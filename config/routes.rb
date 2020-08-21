c65f40dd62a00842a7c2b693780e028e61e5b243
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-22 02:36:42 +0900

    html修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'items#index'
   post "items/:item_id/add" , to: "carts#add" , as: 'item_add_to_cart'
-  get "items/:item_id/remove" , to: "carts#remove" , as: 'item_remove_from_cart'
+  delete "items/:item_id/remove" , to: "carts#remove" , as: 'item_remove_from_cart'
   resources :carts, only: [:index]
   resources :orders, only: [:index, :new, :create]
 end
