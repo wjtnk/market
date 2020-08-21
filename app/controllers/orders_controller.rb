a0153578c66412c816681d65d257310e4d99ce6a
wjtnk <r.sugikawa@sonicmoov.com>
2020-08-21 22:57:57 +0900

    購入履歴を逆順に

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = Order.where(user_id: current_user.id)
+    @orders = Order.where(user_id: current_user.id).order(id: "DESC")
   end
 
   def new
