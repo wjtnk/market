4c5a56e3bb6404c1bf1ffeb3c5432ab3c1ed725c
wjtnk <r.sugikawa@sonicmoov.com>
2020-09-20 17:22:27 +0900

    CartsControllerリファクタ

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,12 +6,29 @@ class CartsController < ApplicationController
   end
 
   def create
-    Cart.add_item(current_user.id, params[:item_id])
+    cart = Cart.find_by(user_id: current_user.id, item_id: params[:item_id])
+
+    if cart.blank?
+      # itemが新しいものだったら「count: 1」として新規作成してreturn
+      Cart.create!(user_id: current_user.id, item_id: params[:item_id], count: 1)
+    else
+      # countに+1して更新
+      cart.update(count: cart.count += 1)
+    end
+
     redirect_to carts_path, notice: "カートに商品を追加しました!!"
   end
 
   def remove
-    Cart.remove_item(current_user.id, params[:item_id])
+    cart = Cart.find_by(user_id: current_user.id, item_id: params[:item_id])
+
+    # cart.countが2個以上ある時はcountを1つ減らす
+    if cart.count >= 2
+      cart.update(count: cart.count -= 1)
+    else
+      cart.destroy
+    end
+
     redirect_back fallback_location: root_path, notice: "カートから商品を削除しました!!"
   end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,35 +11,4 @@ class Cart < ApplicationRecord
     count
   end
 
-  # カートにitemを追加
-  def self.add_item(user_id, item_id)
-    cart = self.find_by(user_id: user_id, item_id:item_id)
-
-    if cart.blank?
-      # itemが新しいものだったら「count: 1」として新規作成してreturn
-      self.create!(
-          user_id: user_id,
-          item_id: item_id,
-          count: 1
-      )
-      return
-    end
-
-    # countに+1して更新
-    cart.update(count: cart.count += 1)
-  end
-
-  # カートからItemを削除
-  def self.remove_item(user_id, item_id)
-    cart = self.find_by(user_id: user_id, item_id:item_id)
-
-    # cart.countが2個以上ある時はcountを1つ減らしてreturn
-    if cart.count >= 2
-      cart.update(count: cart.count -= 1)
-      return
-    end
-
-    cart.destroy
-  end
-
 end
