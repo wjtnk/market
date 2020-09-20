668c86b3ebed119d6fcc23df139ad8e46c4467a6
wjtnk <r.sugikawa@sonicmoov.com>
2020-09-21 00:07:34 +0900

    price関連のメソッド修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,10 +15,10 @@ class Order < ApplicationRecord
     @carts      = Cart.where(user_id: user_id)
     @item_count = Cart.get_item_count(@carts)
 
-    @item_total_price     = get_item_total_price(@carts)
-    @delivery_fee         = get_delivery_fee(@item_count)
-    @cash_on_delivery_fee = get_cash_on_delivery_fee(@item_total_price)
-    @order_total_price    = get_order_total_price(@item_total_price, @delivery_fee, @cash_on_delivery_fee)
+    @item_total_price     = get_item_total_price
+    @delivery_fee         = get_delivery_fee
+    @cash_on_delivery_fee = get_cash_on_delivery_fee
+    @order_total_price    = get_order_total_price
   end
 
   def purchase
@@ -49,28 +49,28 @@ class Order < ApplicationRecord
   end
 
   # 商品合計金額
-  def get_item_total_price(carts)
+  def get_item_total_price
     item_total_price = 0
-    carts.each do |cart|
+    @carts.each do |cart|
       item_total_price += cart.item.price * cart.count
     end
     item_total_price
   end
 
   #送料算出(5商品ごとに600円追加)
-  def get_delivery_fee(item_count)
-    ( (item_count / 6) + 1 ) * 600
+  def get_delivery_fee
+    ( (@item_count / 6) + 1 ) * 600
   end
 
   #代引き手数料算出
-  def get_cash_on_delivery_fee(item_total_price)
-    if 0 <= item_total_price && item_total_price < 10000
+  def get_cash_on_delivery_fee
+    if 0 <= @item_total_price && @item_total_price < 10000
       300
-    elsif 10000 <= item_total_price && item_total_price < 30000
+    elsif 10000 <= @item_total_price && @item_total_price < 30000
       400
-    elsif 30000 <= item_total_price && item_total_price < 100000
+    elsif 30000 <= @item_total_price && @item_total_price < 100000
       600
-    elsif 100000 < item_total_price
+    elsif 100000 < @item_total_price
       1000
     else
       0
@@ -78,8 +78,8 @@ class Order < ApplicationRecord
   end
 
   # 商品の税込み合計金額
-  def get_order_total_price(item_total_price, delivery_fee, cash_on_delivery_fee)
-    ((item_total_price + delivery_fee + cash_on_delivery_fee) * 1.08).floor
+  def get_order_total_price
+    ((@item_total_price + @delivery_fee + @cash_on_delivery_fee) * 1.08).floor
   end
 
 end
