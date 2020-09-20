86ff660104e87b2734304096d89a98d323aed503
wjtnk <r.sugikawa@sonicmoov.com>
2020-09-20 17:02:38 +0900

    create_orderをインスタンスメソッドに

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,17 @@ class OrdersController < ApplicationController
     @carts      = Cart.where(user_id: current_user.id)
     @item_count = Cart.get_item_count(@carts)
 
-    @item_total_price     = Order.get_item_total_price(@carts)
-    @delivery_fee         = Order.get_delivery_fee(@item_count)
-    @cash_on_delivery_fee = Order.get_cash_on_delivery_fee(@item_total_price)
-    @order_total_price    = Order.get_order_total_price(@item_total_price, @delivery_fee, @cash_on_delivery_fee)
+    order = Order.new
+    @item_total_price     = order.get_item_total_price(@carts)
+    @delivery_fee         = order.get_delivery_fee(@item_count)
+    @cash_on_delivery_fee = order.get_cash_on_delivery_fee(@item_total_price)
+    @order_total_price    = order.get_order_total_price(@item_total_price, @delivery_fee, @cash_on_delivery_fee)
   end
 
   # カート(キャッシュ)に保存している商品を購入
   def create
-    Order.create_order(current_user.id, params[:address], params[:deliver_time])
+    order = Order.new
+    order.create_order(current_user.id, params[:address], params[:deliver_time])
     redirect_to orders_path, notice: '購入ありがとうございます!!'
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,19 +6,19 @@ class Order < ApplicationRecord
   DELIVER_TIME = { "8-12" => 1, "12-14" => 2, "14-16" => 3, "16-18" => 4, "18-20" => 5, "20-21" => 6 }
 
   # 商品を購入
-  def self.create_order(user_id, address, deliver_time)
+  def create_order(user_id, address, deliver_time)
     ActiveRecord::Base.transaction do
 
       carts      = Cart.where(user_id: user_id)
       item_count = Cart.get_item_count(carts)
 
-      item_total_price     = self.get_item_total_price(carts)
-      delivery_fee         = self.get_delivery_fee(item_count)
-      cash_on_delivery_fee = self.get_cash_on_delivery_fee(item_total_price)
-      order_total_price    = self.get_order_total_price(item_total_price, delivery_fee, cash_on_delivery_fee)
+      item_total_price     = get_item_total_price(carts)
+      delivery_fee         = get_delivery_fee(item_count)
+      cash_on_delivery_fee = get_cash_on_delivery_fee(item_total_price)
+      order_total_price    = get_order_total_price(item_total_price, delivery_fee, cash_on_delivery_fee)
 
       #注文を作成
-      order = self.create!(
+      order = Order.create!(
           delivery_fee: delivery_fee,
           cash_on_delivery_fee: cash_on_delivery_fee,
           total_price: order_total_price,
@@ -43,7 +43,7 @@ class Order < ApplicationRecord
   end
 
   # 商品合計金額
-  def self.get_item_total_price(carts)
+  def get_item_total_price(carts)
     item_total_price = 0
     carts.each do |cart|
       item_total_price += cart.item.price * cart.count
@@ -52,12 +52,12 @@ class Order < ApplicationRecord
   end
 
   #送料算出(5商品ごとに600円追加)
-  def self.get_delivery_fee(item_count)
+  def get_delivery_fee(item_count)
     ( (item_count / 6) + 1 ) * 600
   end
 
   #代引き手数料算出
-  def self.get_cash_on_delivery_fee(item_total_price)
+  def get_cash_on_delivery_fee(item_total_price)
     if 0 <= item_total_price && item_total_price < 10000
       300
     elsif 10000 <= item_total_price && item_total_price < 30000
@@ -72,7 +72,7 @@ class Order < ApplicationRecord
   end
 
   # 商品の税込み合計金額
-  def self.get_order_total_price(item_total_price, delivery_fee, cash_on_delivery_fee)
+  def get_order_total_price(item_total_price, delivery_fee, cash_on_delivery_fee)
     ((item_total_price + delivery_fee + cash_on_delivery_fee) * 1.08).floor
   end
 
