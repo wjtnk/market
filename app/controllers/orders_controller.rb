3963b41387295413a1e11d5625033d2e805185f0
wjtnk <r.sugikawa@sonicmoov.com>
2020-09-24 17:43:59 +0900

    OrdersControllerのindex修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = Order.where(user_id: current_user.id).order(id: "DESC")
+    @orders = current_user.orders.where(user_id: current_user.id).order(id: "DESC")
   end
 
   def new
