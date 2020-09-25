1aec3da1c8ad0c2a52646639cc05b8434b1abb98
wjtnk <r.sugikawa@sonicmoov.com>
2020-09-25 11:48:10 +0900

    修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @carts = current_user.carts
+    @carts = current_user.cart.cart_items
   end
 
   def add_item
