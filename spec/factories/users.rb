9c1603eac200f4d30b01eb552efd77c2de06cc9e
wjtnk <r.sugikawa@sonicmoov.com>
2020-10-18 01:09:35 +0900

    userテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "sample#{n}@example.com" }
+    name "sample太郎"
+    default_address "新宿区西新宿2-8-1"
+    default_deliver_time 1
+    password "password"
   end
 end
