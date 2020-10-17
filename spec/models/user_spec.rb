243cc1a49e0e429ee6fbf044ec92f35082bead64
wjtnk <r.sugikawa@sonicmoov.com>
2020-10-18 01:12:41 +0900

    userテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe User, type: :model do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
+  it "Emailがないとユーザーを作成できないこと" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "Emailが重複していればユーザー作成できないこと" do
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
 end
