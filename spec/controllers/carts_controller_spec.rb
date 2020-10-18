3d101c009f5cc9dd6c00a91d3a6dd02c32327741
wjtnk <r.sugikawa@sonicmoov.com>
2020-10-18 15:46:51 +0900

    cartコントローラーテスト

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe CartsController, type: :controller do
     end
   end
 
+  describe "#add_item" do
+    it "商品を追加できること" do
+      item = FactoryBot.create(:item)
+      user = FactoryBot.create(:user)
+      sign_in user
+      post :add_item, params: { item_id: item.id }
+      expect(response).to redirect_to carts_path
+    end
+  end
+
+  # describe "#remove_item" do
+  #   it "商品を追加できること" do
+  #     item = FactoryBot.create(:item)
+  #     user = FactoryBot.create(:user)
+  #     sign_in user
+  #     post :add_item, params: { item_id: item.id }
+  #     expect(response).to redirect_to carts_path
+  #   end
+  # end
+
 end
